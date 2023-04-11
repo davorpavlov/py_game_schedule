0dbf3610f2a533c9a9f6967e369b8e70e3e6cdb6
davorpavlov <pavlovic.davor91@gmail.com>
2023-04-11 13:00:32 +0200

    Create py_game_schedule.py

---
+++ py_game_schedule.py
@@ -0,0 +1,18 @@
+from datetime import date, timedelta
+
+class Season:
+    def __init__(self, start_date):
+        self.start_date = start_date
+        self.end_date = start_date + timedelta(weeks=26)
+
+    def print_game_schedule(self):
+        for i in range(0, 26, 2):
+            wednesday = self.start_date + timedelta(weeks=i, days=0)
+            sunday = self.start_date + timedelta(weeks=i, days=4)
+            print("Utakmica - Srijeda: ", wednesday)
+            print("Utakmica - Nedjelja: ", sunday)
+
+start_date = date.today() + timedelta(days=1)  
+
+season = Season(start_date)
+season.print_game_schedule()
